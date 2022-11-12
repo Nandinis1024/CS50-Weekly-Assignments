7ebd860815d3c3afc15b25f590079619baa167ae
Nandinis1024 <114241516+Nandinis1024@users.noreply.github.com>
2022-11-12 07:56:28 +0000

    Sat, Nov 12, 2022, 7:56 AM +00:00

--- mario-more/mario.c
+++ mario-more/mario.c
@@ -1,7 +1,45 @@
 #include <cs50.h>
 #include <stdio.h>
 
-int main(void) 
+int main(void)
 {
-    
+     int height;
+    printf("enter height:\n");
+    scanf("%d",&height);
+    while(true){
+    if(height<1||height>8){
+
+      printf("OOPS WRONG INPUT,enter height AGAIN:\n");
+      scanf("%d",&height);
+
+    }
+
+    else{
+       break;
+    }
+    }
+
+
+    int stored= height;
+   for(int i=1;i<=stored;i++){
+    for(int j=1;j<=(stored-i);j++){
+      printf(" ");
+    }
+    int k=1;
+    for(;k<=i;k++){
+      printf("#");
+    }
+    int m=1;
+    for(;m<=2;m++){
+        printf(" ");
+    }
+    int n=1;
+    for(;n<=i;n++){
+        printf("#");
+    }
+
+    printf("\n");
+
+
+   }
 }
