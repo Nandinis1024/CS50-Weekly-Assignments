9f12e62246fa33939b9f6f4be0fc81508ca1bf41
Nandinis1024 <114241516+Nandinis1024@users.noreply.github.com>
2022-11-20 19:07:57 +0000

    Sun, Nov 20, 2022, 7:07 PM +00:00

--- readability/readability.c
+++ readability/readability.c
@@ -26,20 +26,20 @@ int main(void)
     float S = sentences / words * 100;
     float index = (0.0588 * L) - (0.296 * S) - 15.8;
     int grade = round(index);
-    if(grade<1)
+    if (grade < 1)
     {
-    printf("Before Grade 1\n");
+        printf("Before Grade 1\n");
 
     }
-    else if(grade>16)
+    else if (grade > 16)
     {
-    printf("Grade 16+\n");
+        printf("Grade 16+\n");
     }
     else
     {
-        printf("Grade %d\n",grade);
+        printf("Grade %d\n", grade);
     }
-    }
+}
 
 
     float count_letters(string text)
