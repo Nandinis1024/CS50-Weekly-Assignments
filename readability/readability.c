e9f6ec3a791252025d5626e8d186d6a1a5f31c45
Nandinis1024 <114241516+Nandinis1024@users.noreply.github.com>
2022-11-20 19:17:27 +0000

    Sun, Nov 20, 2022, 7:17 PM +00:00

--- readability/readability.c
+++ readability/readability.c
@@ -47,7 +47,7 @@ float count_letters(string text)
     //this function counts and returns the number of letters in a text to the main function
     int count =  0;
     int length = strlen(text);
-    for(int i = 0; i < length; i++)
+    for (int i = 0; i < length; i++)
     {
         char c = text[i];
         if (isalpha(c))
