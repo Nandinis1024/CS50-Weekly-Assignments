cf17dff4c62eb3c47e4a3673366ec46e164ec970
Nandinis1024 <114241516+Nandinis1024@users.noreply.github.com>
2022-11-20 09:57:58 +0000

    Sun, Nov 20, 2022, 9:57 AM +00:00

--- readability/readability.c
+++ readability/readability.c
@@ -1,5 +1,7 @@
 #include <cs50.h>
 #include <stdio.h>
+#include <string.h>
+#include <ctype.h>
 
 int count_letters(text);
 int main(void)
@@ -11,5 +13,8 @@ int letters = count_letters(text);
 
 }
 int count_letters(string text){
-    
+int length = strlen(text);
+for(int i=0;i<length;i++){
+    if()
+}
 }
