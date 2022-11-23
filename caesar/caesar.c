844caabc1c414b35f9e91fb52dd08ead3f0a8047
Nandinis1024 <114241516+Nandinis1024@users.noreply.github.com>
2022-11-23 18:24:40 +0000

    Wed, Nov 23, 2022, 6:24 PM +00:00

--- caesar/caesar.c
+++ caesar/caesar.c
@@ -5,19 +5,19 @@
 #include <stdlib.h>
 int main(int argc, string argv[])
 {
-if(argc != 2)
-{
-    printf("Usage: ./caesar key\n");
-    return 1;
-}
-for(int i = 0; argv[1][i]!='\0'; i++)
-{
-    if(isdigit(argv[1][i])==0)
+    if(argc != 2)
     {
         printf("Usage: ./caesar key\n");
         return 1;
-
     }
+    for(int i = 0; argv[1][i]!='\0'; i++)
+    {
+        if(isdigit(argv[1][i])==0)
+        {
+            printf("Usage: ./caesar key\n");
+            return 1;
+
+        }
 }
 
     int key = atoi(argv[1]);
