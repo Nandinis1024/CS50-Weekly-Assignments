987b5098f1d215c59d48588aa450e50212941f09
Nandinis1024 <114241516+Nandinis1024@users.noreply.github.com>
2022-11-23 14:19:40 +0000

    Wed, Nov 23, 2022, 2:19 PM +00:00

--- caesar/caesar.c
+++ caesar/caesar.c
@@ -4,12 +4,12 @@
 #include <string.h>
 int main(int argc, string argv[])
 {
-if(argc!=2)
+if(argc!=2||)
 {
     printf("Usage: ./caesar key\n");
     return 1;
 }
-int len =
+int len = strlen(ar)
 else
 {
     printf("%s\n",argv[1]);
