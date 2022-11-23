c7f0cb455de1d474d167777bf856b02f8306bf74
Nandinis1024 <114241516+Nandinis1024@users.noreply.github.com>
2022-11-23 16:15:40 +0000

    Wed, Nov 23, 2022, 4:15 PM +00:00

--- caesar/caesar.c
+++ caesar/caesar.c
@@ -9,7 +9,11 @@ if(argc == 2||isdigit(*argv[1]))
 {
     int key = atoi(argv[1]);
     string plaintext = get_string("plaintext:  ");
-    
+    int length = strlen(plaintext);
+    for(int i = 0; i < length; i++)
+    {
+        int ciphertext[i]=
+    }
 
 
     return 0;
