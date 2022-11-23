60def50c0a1abf33d6dce563133df48066e9161d
Nandinis1024 <114241516+Nandinis1024@users.noreply.github.com>
2022-11-23 18:28:46 +0000

    Wed, Nov 23, 2022, 6:28 PM +00:00

--- caesar/caesar.c
+++ caesar/caesar.c
@@ -5,41 +5,41 @@
 #include <stdlib.h>
 int main(int argc, string argv[])
 {
-    if(argc != 2)
+    if (argc != 2)
     {
         printf("Usage: ./caesar key\n");
         return 1;
     }
-    for(int i = 0; argv[1][i]!='\0'; i++)
+    for (int i = 0; argv[1][i] != '\0'; i++)
     {
-        if(isdigit(argv[1][i])==0)
+        if (isdigit(argv[1][i])  == 0)
         {
             printf("Usage: ./caesar key\n");
             return 1;
 
         }
-}
+    }
 
     int key = atoi(argv[1]);
     string plaintext = get_string("plaintext:  ");
     printf("ciphertext: ");
     int length = strlen(plaintext);
-    for(int i = 0; i < length; i++)
+    for (int i = 0; i < length; i++)
     {
         char c = plaintext[i];
-        if(islower(c))
+        if (islower(c))
         {
-            printf("%c",(((c-'a')+ key)%26)+'a');
+            printf("%c", (((c - 'a') + key) % 26) + 'a');
 
         }
         else if(isupper(c))
         {
-            printf("%c",(((c-'A')+ key)%26)+'A');
+            printf("%c", (((c - 'A') + key) %26) + 'A');
 
         }
         else
         {
-            printf("%c",c);
+            printf("%c", c);
         }
     }
     printf("\n");
