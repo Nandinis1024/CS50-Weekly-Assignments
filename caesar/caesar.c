41188eeea7a7aa41b9791d01daceb7e0a5e47d9a
Nandinis1024 <114241516+Nandinis1024@users.noreply.github.com>
2022-11-23 14:17:54 +0000

    Wed, Nov 23, 2022, 2:17 PM +00:00

--- caesar/caesar.c
+++ caesar/caesar.c
@@ -1,6 +1,7 @@
 #include <cs50.h>
 #include <stdio.h>
 #include <ctype.h>
+#include <string.h>
 int main(int argc, string argv[])
 {
 if(argc!=2)
@@ -8,7 +9,7 @@ if(argc!=2)
     printf("Usage: ./caesar key\n");
     return 1;
 }
-
+int len =
 else
 {
     printf("%s\n",argv[1]);
