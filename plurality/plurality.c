5e39f5d1d23417191d8c5d086fad80d71786f2ef
Nandinis1024 <114241516+Nandinis1024@users.noreply.github.com>
2022-11-26 09:21:56 +0000

    Sat, Nov 26, 2022, 9:21 AM +00:00

--- plurality/plurality.c
+++ plurality/plurality.c
@@ -68,11 +68,11 @@ bool vote(string name)
 {
     for (int i = 0; i < candidate_count; i++)
     {
-     if (strcmp(candidates[i].name, name) == 0)
-     {
-         candidates[i].votes++;
-         return true;
-     }
+         if (strcmp(candidates[i].name, name) == 0)
+         {
+             candidates[i].votes++;
+             return true;
+         }
     }
         return false;
 }
