07623c55c1d17d7844fa0f81edd659dd9a6ba6a8
Nandinis1024 <114241516+Nandinis1024@users.noreply.github.com>
2022-11-26 09:19:16 +0000

    Sat, Nov 26, 2022, 9:19 AM +00:00

--- plurality/plurality.c
+++ plurality/plurality.c
@@ -83,14 +83,14 @@ void print_winner(void)
     int max_votes = 0;
     for (int i = 0; i < candidate_count; i++)
     {
-        if(candidates[i].votes > max_votes)
+        if (candidates[i].votes > max_votes)
         {
             max_votes = candidates[i].votes;
         }
 
     }
 
-     for (int i = 0; i < candidate_count; i++)
+    for (int i = 0; i < candidate_count; i++)
     {
 
         if (candidates[i].votes == max_votes)
