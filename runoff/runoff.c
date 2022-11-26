0656dea46bd507bc9ff404154fee22d0c660154c
Nandinis1024 <114241516+Nandinis1024@users.noreply.github.com>
2022-11-26 10:39:44 +0000

    Sat, Nov 26, 2022, 10:39 AM +00:00

--- runoff/runoff.c
+++ runoff/runoff.c
@@ -127,7 +127,13 @@ int main(int argc, string argv[])
 // Record preference if vote is valid
 bool vote(int voter, int rank, string name)
 {
-    
+    for (int i = 0; i < candidates_count; i++)
+    {
+        if (strcmp(candidates[i].name, name)==0)
+        {
+            return true;
+        }
+    }
     return false;
 }
 
