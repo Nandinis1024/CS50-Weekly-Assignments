ad3ab035b9e657059d834910397daa750688ec71
Nandinis1024 <114241516+Nandinis1024@users.noreply.github.com>
2022-11-27 11:37:30 +0000

    Sun, Nov 27, 2022, 11:37 AM +00:00

--- runoff/runoff2.c
+++ runoff/runoff2.c
@@ -1,10 +1,14 @@
 #include<stdio.h>
 #include<cs50.h>
 
+#define MAX_VOTERS 100
+#define MAX_CANDIDATES 9
+
 int main(int argc, string argv[])
 {
     int voters = get_int("number of voters: ");
-    
+    int candidate_count = argc-1;
+    for(int i = 0; i < )
 
 
 
