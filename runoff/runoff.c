05673b582c8971356424851d4cd194bf285ab5de
Nandinis1024 <114241516+Nandinis1024@users.noreply.github.com>
2022-11-27 08:25:11 +0000

    Sun, Nov 27, 2022, 8:25 AM +00:00

--- runoff/runoff.c
+++ runoff/runoff.c
@@ -179,7 +179,7 @@ int find_min(void)
     int min_votes=voter_count;
     for(int i=0;i<candidate_count;i++)
     {
-        if(candidates[i].eliminated==false && candidates[i].vote < min_votes)
+        if(candidates[i].eliminated==false && candidates[i].votes < min_votes)
         {
              min_votes=candidates[i].votes;
 
