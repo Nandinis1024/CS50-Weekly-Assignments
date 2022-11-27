863137436414dc39aab93526d65a260cbc2aa7e8
Nandinis1024 <114241516+Nandinis1024@users.noreply.github.com>
2022-11-27 11:41:23 +0000

    Sun, Nov 27, 2022, 11:41 AM +00:00

--- runoff/runoff2.c
+++ runoff/runoff2.c
@@ -1,8 +1,21 @@
 #include<stdio.h>
 #include<cs50.h>
+#include<string.h>
 
 #define MAX_VOTERS 100
 #define MAX_CANDIDATES 9
+
+int preferences[MAX_VOTERS][MAX_CANDIDATES];
+
+typedef struct
+{
+    string name;
+    int votes;
+    bool eliminated;
+}
+candidate;
+int voters
+
 
 int main(int argc, string argv[])
 {
@@ -10,7 +23,7 @@ int main(int argc, string argv[])
     int candidate_count = argc-1;
     for(int i = 0; i < candidate_count; i++)
     {
-        
+
 
     }
 
