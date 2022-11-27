052edcd6f9bc597f5cc2247f4de4f31c1aca643e
Nandinis1024 <114241516+Nandinis1024@users.noreply.github.com>
2022-11-27 08:48:31 +0000

    Sun, Nov 27, 2022, 8:48 AM +00:00

--- runoff/runoff.c
+++ runoff/runoff.c
@@ -164,10 +164,10 @@ void tabulate(void)
 // Print the winner of the election, if there is one
 bool print_winner(void)
 {
-    for(int i=0;i<candidate_count;i++)
+    for (int i = 0; i < candidate_count; i++)
     {
         string most_votes = candidates[i].name;
-        if(candidates[i].votes>(voter_count/2))
+        if (candidates[i].votes > (voter_count/2))
         {
             printf("winner %s\n",most_votes);
             return true;
@@ -179,12 +179,12 @@ bool print_winner(void)
 // Return the minimum number of votes any remaining candidate has
 int find_min(void)
 {
-    int min_votes=voter_count;
-    for(int i=0;i<candidate_count;i++)
+    int min_votes = voter_count;
+    for (int i = 0; i < candidate_count; i++)
     {
-        if(candidates[i].eliminated==false && candidates[i].votes < min_votes)
+        if (candidates[i].eliminated == false && candidates[i].votes < min_votes)
         {
-             min_votes=candidates[i].votes;
+             min_votes = candidates[i].votes;
 
 
         }
@@ -195,9 +195,9 @@ int find_min(void)
 // Return true if the election is tied between all candidates, false otherwise
 bool is_tie(int min)
 {
-    for(int i=0;i<candidate_count;i++)
+    for (int i = 0; i < candidate_count; i++)
      {
-         if(candidates[i].eliminated==false && candidates[i].votes!=min)
+         if (candidates[i].eliminated == false && candidates[i].votes != min)
          {
 
             return false;
@@ -210,10 +210,10 @@ bool is_tie(int min)
 // Eliminate the candidate (or candidates) in last place
 void eliminate(int min)
 {
-    for(int i=0;i<candidate_count;i++) // loop thru candidates
-        if (candidates[i].eliminated==false && candidates[i].votes==min)
+    for (int i = 0; i < candidate_count; i++) // loop thru candidates
+        if (candidates[i].eliminated == false && candidates[i].votes == min)
         {
-            candidates[i].eliminated=true;
+            candidates[i].eliminated = true;
         }
     return;
 }
