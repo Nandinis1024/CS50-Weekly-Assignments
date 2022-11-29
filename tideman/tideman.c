620e2c5363dd4dcb15894c8898fd584a6a2d5793
Nandinis1024 <114241516+Nandinis1024@users.noreply.github.com>
2022-11-29 06:49:05 +0000

    Tue, Nov 29, 2022, 6:49 AM +00:00

--- tideman/tideman.c
+++ tideman/tideman.c
@@ -100,52 +100,20 @@ int main(int argc, string argv[])
 bool vote(int rank, string name, int ranks[])
 {
 
-    for( int i = 0; i < candidate_count; i++)
-    {
-        if (strcmp(candidates[i], name)==0)
-        {
-            ranks[rank] = i;
-            return true;
-        }
-    }
-    return false;
+
 }
 
 // Update preferences given one voter's ranks
 void record_preferences(int ranks[])
 {
-    for (int i = 0; i < candidate_count; i++)
-    {
-        for (int j = i + 1; j < candidate_count; j++)
-        {
-            preferences[rank[i]][rank[j]]++;
-        }
-    }
+
 
 }
 
 // Record pairs of candidates where one is preferred over the other
 void add_pairs(void)
 {
-    for (int i = 0; i < candidate_count; i++)
-    {
-        for (int j = 0; j < candidate_count; j++)
-        {
-            if (preferences[i][j] > preferences[j][i])
-            {
-                pairs[pair_count].winner = i;
-                pairs[pair_count].looser = j;
-                pair_count++;
-            }
-            else if (preferences[i][j] > preferences[j][i])
-            {
-                pairs[pair_count].winner = j;
-                pairs[pair_count].looser = i;
-                pair_count++;
-
-            }
-        }
-    }
+    
 }
 
 // Sort pairs in decreasing order by strength of victory
