69925f16675b245798165525e68960913be5c0f7
Nandinis1024 <114241516+Nandinis1024@users.noreply.github.com>
2022-12-02 20:41:19 +0000

    Fri, Dec 2, 2022, 8:41 PM +00:00

--- filter-more/helpers.c
+++ filter-more/helpers.c
@@ -23,7 +23,7 @@ void reflect(int height, int width, RGBTRIPLE image[height][width])
     {
         for (int j = 0; j < (width/2); j++)
         {
-            BYTE temp = image[i][j];
+            RGBTRIPLE temp = image[i][j];
             image[i][j] = image[i][(width-1)-j];
             image[i][(width-1)-j] = temp;
 
