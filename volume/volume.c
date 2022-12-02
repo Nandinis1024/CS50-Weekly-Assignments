cb30043be8a63dd998d9c6ff2e44d8cb05f3a5fd
Nandinis1024 <114241516+Nandinis1024@users.noreply.github.com>
2022-12-02 09:43:40 +0000

    Fri, Dec 2, 2022, 9:43 AM +00:00

--- volume/volume.c
+++ volume/volume.c
@@ -39,7 +39,8 @@ int main(int argc, char *argv[])
     fwrite(header, HEADER_SIZE, 1, output);
 
     // TODO: Read samples from input file and write updated data to output file
-    
+    int16_t buffer;
+    while (fread(&buffer, sizeof(int16_t)))
 
     // Close files
     fclose(input);
