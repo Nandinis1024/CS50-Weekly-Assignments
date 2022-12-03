8d14cd42159312bc03e65896e766468037ffc609
Nandinis1024 <114241516+Nandinis1024@users.noreply.github.com>
2022-12-03 19:36:35 +0000

    Sat, Dec 3, 2022, 7:36 PM +00:00

--- filter-more/helpers.c
+++ filter-more/helpers.c
@@ -118,9 +118,9 @@ void edges(int height, int width, RGBTRIPLE image[height][width])
 
 
 
-            image[i][j].rgbtBlue = pow(sum_blue_x, 2)+pow(sum_blue_y, 2);
-            image[i][j].rgbtGreen = pow((pow(sum_green_x, 2)+pow(sum_green_y, 2)), 0.5) ;
-            image[i][j].rgbtRed = pow((pow(sum_red_x, 2)+pow(sum_red_y, 2)), 0.5);
+            image[i][j].rgbtBlue = round(sqrt(pow(sum_blue_x, 2)+pow(sum_blue_y, 2)));
+            image[i][j].rgbtGreen = round(sqrt(pow(sum_green_x, 2)+pow(sum_green_y, 2))) ;
+            image[i][j].rgbtRed = round(sqrt(pow(sum_red_x, 2)+pow(sum_red_y, 2)));
 
 
 
