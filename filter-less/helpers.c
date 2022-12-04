51c0f518f2c1ad034ea1fb4271571ea96e3a7fe1
Nandinis1024 <114241516+Nandinis1024@users.noreply.github.com>
2022-12-04 19:01:00 +0000

    Sun, Dec 4, 2022, 7:01 PM +00:00

--- filter-less/helpers.c
+++ filter-less/helpers.c
@@ -65,6 +65,8 @@ void blur(int height, int width, RGBTRIPLE image[height][width])
     {
         for (int j = 0; j < width; j++)
         {
+            temp[i][j] = image[i][j];
+            
 
         }
     }
