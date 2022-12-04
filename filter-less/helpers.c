a090881e7cb0b41ec52484e4556c595180afb222
Nandinis1024 <114241516+Nandinis1024@users.noreply.github.com>
2022-12-04 19:49:35 +0000

    Sun, Dec 4, 2022, 7:49 PM +00:00

--- filter-less/helpers.c
+++ filter-less/helpers.c
@@ -35,9 +35,9 @@ void sepia(int height, int width, RGBTRIPLE image[height][width])
     {
         for (int j = 0; j < width; j++)
         {
-            int sepia_red = round(.393 * image[i][j].rgbtRed + .769 * image[i][j].rgbtGreen + .189 * image[i][j].rgbtBlue)
-            int sepia_green
-            int sepia_blue
+            int sepia_red = round(.393 * image[i][j].rgbtRed + .769 * image[i][j].rgbtGreen + .189 * image[i][j].rgbtBlue);
+            int sepia_green = round(.349 * image[i][j].rgbtRed + .686 * image[i][j].rgbtGreen + .168 * image[i][j].rgbtBlue);
+            int sepia_blue = round(.272 * image[i][j].rgbtRed + .534 * image[i][j].rgbtGreen + .131 * image[i][j].rgbtBlue);
         }
     }
     return;
