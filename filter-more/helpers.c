8b92eba738923eb9c5d83a1cae591813da2130f5
Nandinis1024 <114241516+Nandinis1024@users.noreply.github.com>
2022-12-04 16:16:10 +0000

    Sun, Dec 4, 2022, 4:16 PM +00:00

--- filter-more/helpers.c
+++ filter-more/helpers.c
@@ -8,7 +8,7 @@ void grayscale(int height, int width, RGBTRIPLE image[height][width])
     {
         for (int j = 0; j < width; j++)
         {
-            float avg = ((image[i][j].rgbtBlue + image[i][j].rgbtGreen + image[i][j].rgbtRed)/3);
+            int avg = round((image[i][j].rgbtBlue + image[i][j].rgbtGreen + image[i][j].rgbtRed)/3);
             image[i][j].rgbtBlue = avg;
             image[i][j].rgbtGreen = avg;
             image[i][j].rgbtRed = avg;
