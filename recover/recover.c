dd31b7cd2d2f118819cfad9c1732704df394fea3
Nandinis1024 <114241516+Nandinis1024@users.noreply.github.com>
2022-12-10 16:56:57 +0000

    Sat, Dec 10, 2022, 4:56 PM +00:00

--- recover/recover.c
+++ recover/recover.c
@@ -31,7 +31,7 @@ char filename[8];
 typedef uint8_t BYTE;
 BYTE buffer[BLOCK_SIZE];
 while (fread(buffer, 1, BLOCK_SIZE, file) == BLOCK_SIZE)
-{                                                                                                                       
+{
     if(buffer[0]==0xff && buffer[1] == 0xd8 && buffer[2] == 0Xff && (buffer[3] & 0Xf0) == 0Xe0)
     {
         if(img != NULL)
@@ -46,17 +46,17 @@ while (fread(buffer, 1, BLOCK_SIZE, file) == BLOCK_SIZE)
 
 
     }
-    if (img != 1)
+    if (img != NULL)
         {
 
-            fwrite(&buffer, BLOCK_SIZE, 1, picture);
+            fwrite(&buffer, BLOCK_SIZE, 1, img);
         }
 
 
 
 }
-    fclose(input);
-    fclose(picture);
+    fclose(file);
+    fclose(img);
 
     return 0;
 }
