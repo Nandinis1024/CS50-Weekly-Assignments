c9813b680b33d46d1f78b43112b30cd1da756a32
Nandinis1024 <114241516+Nandinis1024@users.noreply.github.com>
2022-12-10 09:13:20 +0000

    Sat, Dec 10, 2022, 9:13 AM +00:00

--- recover/recover.c
+++ recover/recover.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+// Number of bytes in .wav header
+const int BLOCK_SIZE = 512;
+
 int main(int argc, char *argv[])
 {
     //checking the command-line arguments
