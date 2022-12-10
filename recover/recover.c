6523a609e8a7f7c5246536cc744bec97ab5440ef
Nandinis1024 <114241516+Nandinis1024@users.noreply.github.com>
2022-12-10 16:26:10 +0000

    Sat, Dec 10, 2022, 4:26 PM +00:00

--- recover/recover.c
+++ recover/recover.c
@@ -24,6 +24,8 @@ if(file == NULL)
     printf("Could not open card.raw.\n");
     return 2;
 }
+FILE*img = NULL;
+
 
 typedef uint8_t BYTE;
 BYTE buffer[BLOCK_SIZE];
@@ -31,6 +33,7 @@ while (fread(buffer, 1, BLOCK_SIZE, file) == BLOCK_SIZE)
 {
     if(buffer[0]==0xff && buffer[1] == 0xd8 && buffer[2] == 0Xff && (buffer[3] & 0Xf0) == 0Xe0 )
     {
+
 
     }
 
