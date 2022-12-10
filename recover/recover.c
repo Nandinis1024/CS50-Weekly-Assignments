bf9e970cf698f43851ce2dc86cfecf17c6ba331d
Nandinis1024 <114241516+Nandinis1024@users.noreply.github.com>
2022-12-10 10:18:45 +0000

    Sat, Dec 10, 2022, 10:18 AM +00:00

--- recover/recover.c
+++ recover/recover.c
@@ -18,13 +18,14 @@ int main(int argc, char *argv[])
 }
 //opening the memory card
 FILE*file = fopen(argv[1], "r");
+if(file == NULL)
 typedef uint8_t BYTE;
 BYTE buffer[BLOCK_SIZE];
 while (fread(buffer, 1, BLOCK_SIZE, file) == BLOCK_SIZE)
 {
     if(buffer[0]==0xff && buffer[1] == 0xd8 && buffer[2] == 0Xff && (buffer[3] & 0Xf0) == 0Xe0 )
     {
-        
+
     }
 
 
