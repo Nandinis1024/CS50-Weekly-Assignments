2a88f4254e2dcddf1d1caeaee66201f8fdcb2ad0
Nandinis1024 <114241516+Nandinis1024@users.noreply.github.com>
2022-12-10 08:47:29 +0000

    Sat, Dec 10, 2022, 8:47 AM +00:00

--- recover/recover.c
+++ recover/recover.c
@@ -4,10 +4,11 @@
 int main(int argc, char *argv[])
 {
     //checking the command-line arguments
- if (argc != 2)
- {
-    printf("Usage: ./recover IMAGE\n");
-    return 1;
- }
+    if (argc != 2)
+    {
+       printf("Usage: ./recover IMAGE\n");
+       return 1;
+    }
 
 }
+//opening the memory card
