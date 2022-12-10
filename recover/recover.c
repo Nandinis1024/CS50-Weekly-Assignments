bbfcfff2347d98d78464f319848a0fc5911997b1
Nandinis1024 <114241516+Nandinis1024@users.noreply.github.com>
2022-12-10 09:19:38 +0000

    Sat, Dec 10, 2022, 9:19 AM +00:00

--- recover/recover.c
+++ recover/recover.c
@@ -20,4 +20,8 @@ int main(int argc, char *argv[])
 FILE*file = fopen(argv[1], "r");
 typedef uint8_t BYTE;
 BYTE buffer[BLOCK_SIZE];
+while (fread(buffer, 1, BLOCK_SIZE, raw_file) == BLOCK_SIZE)
+{
 
+
+}
