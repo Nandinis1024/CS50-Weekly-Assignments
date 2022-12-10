9c24e98ac62740752b0bf2bdbed1138792cea0c5
Nandinis1024 <114241516+Nandinis1024@users.noreply.github.com>
2022-12-10 10:08:52 +0000

    Sat, Dec 10, 2022, 10:08 AM +00:00

--- recover/recover.c
+++ recover/recover.c
@@ -23,6 +23,9 @@ BYTE buffer[BLOCK_SIZE];
 while (fread(buffer, 1, BLOCK_SIZE, file) == BLOCK_SIZE)
 {
     if(buffer[0]==0xff && buffer[1] == 0xd8 && buffer[2] == 0Xff && (buffer[3] & 0Xf0) == 0Xe0 )
+    {
+        
+    }
 
 
 }
