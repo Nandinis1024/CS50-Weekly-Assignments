4de330b43eedd3f6ead38ebf2c839ca5b6597c82
Nandinis1024 <114241516+Nandinis1024@users.noreply.github.com>
2022-12-10 10:22:48 +0000

    Sat, Dec 10, 2022, 10:22 AM +00:00

--- recover/recover.c
+++ recover/recover.c
@@ -20,8 +20,10 @@ int main(int argc, char *argv[])
 FILE*file = fopen(argv[1], "r");
 if(file == NULL)
 {
-    
+    printf("Could not open card.raw.\n");
+    return 2;
 }
+
 typedef uint8_t BYTE;
 BYTE buffer[BLOCK_SIZE];
 while (fread(buffer, 1, BLOCK_SIZE, file) == BLOCK_SIZE)
