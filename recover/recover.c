d735b7071f2cf66adc64e5d613ba56a3092d0dc4
Nandinis1024 <114241516+Nandinis1024@users.noreply.github.com>
2022-12-10 17:21:55 +0000

    Sat, Dec 10, 2022, 5:21 PM +00:00

--- recover/recover.c
+++ recover/recover.c
@@ -18,13 +18,13 @@ int main(int argc, char *argv[])
 
 
 //opening the memory card
-FILE*file = fopen(argv[1], "r");
-if(file == NULL)
+FILE *file = fopen(argv[1], "r");
+if (file == NULL)
 {
     printf("Could not open card.raw.\n");
     return 2;
 }
-FILE*img = NULL;
+FILE *img = NULL;
 int file_count = 0;
 char filename[8] = {0};
 
@@ -32,9 +32,9 @@ typedef uint8_t BYTE;
 BYTE buffer[BLOCK_SIZE];
 while (fread(buffer, 1, BLOCK_SIZE, file) == BLOCK_SIZE)
 {
-    if(buffer[0]==0xff && buffer[1] == 0xd8 && buffer[2] == 0Xff && (buffer[3] & 0Xf0) == 0Xe0)
+    if (buffer[0] == 0xff && buffer[1] == 0xd8 && buffer[2] == 0Xff && (buffer[3] & 0Xf0) == 0Xe0)
     {
-        if(img != NULL)
+        if (img != NULL)
         {
             fclose(img);
         }
