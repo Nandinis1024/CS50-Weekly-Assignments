ebf5e641b36b0eb6f2a50b91e51a02787634b2a7
Nandinis1024 <114241516+Nandinis1024@users.noreply.github.com>
2022-12-25 19:54:45 +0000

    Sun, Dec 25, 2022, 7:54 PM +00:00

--- speller/dictionary.c
+++ speller/dictionary.c
@@ -51,16 +51,17 @@ bool check(const char *word)
 unsigned int hash(const char *word)
 {
     // TODO: Improve this hash function
-
-    // Function should take a string and return an index
-    // This hash function adds the ASCII values of all characters in     the word together
-    long sum = 0;
-    for (int i = 0; i < strlen(word); i++)
+    if (islower(word[0]))
     {
-        sum += tolower(word[i]);
-    }
-    return sum % N;
+        return word[0] - 'a';
 
+    }
+    else if (isupper(word[0]))
+    {
+        return word[0] - 'A';
+
+    }
+    return toupper(word[0]) - 'A';
 }
 
 // Loads dictionary into memory, returning true if successful, else false
@@ -82,7 +83,7 @@ bool load(const char *dictionary)
         node *newnode = malloc(sizeof(node));
         if (newnode == NULL)
         {
-
+            
             return false;
         }
 
