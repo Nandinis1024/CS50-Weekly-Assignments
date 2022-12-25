ff136bc4d2dd01c1ba36ed8776aec4768a40d015
Nandinis1024 <114241516+Nandinis1024@users.noreply.github.com>
2022-12-25 09:48:48 +0000

    Sun, Dec 25, 2022, 9:48 AM +00:00

--- speller/dictionary.c
+++ speller/dictionary.c
@@ -120,7 +120,15 @@ bool unload(void)
 {
     for (int i = 0; i < N; i++)
     {
-        
+        node *cursor = table[i];
+        while (cursor != NULL)
+        {
+            node *temp = cursor;
+            cursor = cursor->next;
+            free(temp);
+
+        }
+        free(cursor);
 
     }
 
