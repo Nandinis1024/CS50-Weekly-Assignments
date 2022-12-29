697f13f49caab1e28e5e16878d5798fd0a85a794
Nandinis1024 <114241516+Nandinis1024@users.noreply.github.com>
2022-12-29 10:53:12 +0000

    Thu, Dec 29, 2022, 10:53 AM +00:00

--- world-cup/tournament.py
+++ world-cup/tournament.py
@@ -17,6 +17,15 @@ def main():
     teams = []
     # TODO: Read teams into memory from file
 
+    filename = "/workspaces/114241516/world-cup/2018m.csv"
+    with open(filename) as file:
+        reader = csv.DictReader(file)
+        for row in reader:
+            teams.append(row)
+            
+
+
+
     counts = {}
     # TODO: Simulate N tournaments and keep track of win counts
 
