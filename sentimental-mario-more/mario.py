de722a1027d2c6d1a89b47e95452fe8506dc0b00
Nandinis1024 <114241516+Nandinis1024@users.noreply.github.com>
2022-12-29 20:21:58 +0000

    Thu, Dec 29, 2022, 8:21 PM +00:00

--- sentimental-mario-more/mario.py
+++ sentimental-mario-more/mario.py
@@ -2,7 +2,8 @@
 from  cs50 import get_int
 
 while True:
-    height = int(input("Height: "))
+    try:
+        height = int(input("Height: "))
     width = height
     if(height > 0 and  height < 9):
         break
@@ -26,6 +27,8 @@ for rows in range(1,height + 1):
     for col in range(rows):
         print("#",end="")
     print()
+
+
 
 
 
