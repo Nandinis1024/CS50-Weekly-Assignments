f1595a9a84e55af5d71a6d2fc047867f8e273443
Nandinis1024 <114241516+Nandinis1024@users.noreply.github.com>
2022-12-29 20:24:45 +0000

    Thu, Dec 29, 2022, 8:24 PM +00:00

--- sentimental-mario-more/mario.py
+++ sentimental-mario-more/mario.py
@@ -4,29 +4,32 @@ from  cs50 import get_int
 while True:
     try:
         height = int(input("Height: "))
-    width = height
-    if(height > 0 and  height < 9):
-        break
+        width = height
+        if(height > 0 and  height < 9):
+            break
 
 
 
 
 
-for rows in range(1,height + 1):
-    for col in range(width - rows):
-        print(" ",end="")
+    for rows in range(1,height + 1):
+        for col in range(width - rows):
+            print(" ",end="")
 
 
-    for col in range(rows):
-        print("#",end="")
+        for col in range(rows):
+            print("#",end="")
 
 
-    for col in range(2):
-        print(" ",end="")
+        for col in range(2):
+            print(" ",end="")
 
-    for col in range(rows):
-        print("#",end="")
-    print()
+        for col in range(rows):
+            print("#",end="")
+        print()
+
+    except ValueError:
+        print("Please enter a number between 1 and 8. ", end="\n")
 
 
 
