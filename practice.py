1284080d8b0151546b95ada08fd492cf426eaf21
Nandinis1024 <114241516+Nandinis1024@users.noreply.github.com>
2022-12-29 10:48:27 +0000

    Thu, Dec 29, 2022, 10:48 AM +00:00

--- practice.py
+++ practice.py
@@ -6,7 +6,8 @@ filename = "/workspaces/114241516/world-cup/2018m.csv"
 with open(filename) as file:
     reader = csv.DictReader(file)
     for row in reader:
-        print(row)
+        teams.append(row)
+        print(teams)
 
 
 
