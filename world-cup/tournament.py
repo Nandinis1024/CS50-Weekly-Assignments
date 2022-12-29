3d62626ee7070d0d284547fdbe76f017fdffabf3
Nandinis1024 <114241516+Nandinis1024@users.noreply.github.com>
2022-12-29 11:25:27 +0000

    Thu, Dec 29, 2022, 11:25 AM +00:00

--- world-cup/tournament.py
+++ world-cup/tournament.py
@@ -22,6 +22,7 @@ def main():
     with open(filename) as file:
         reader = csv.DictReader(file)
         for row in reader:
+            row["rating"] = int(row["rating"])
             teams.append(row)
 
 
