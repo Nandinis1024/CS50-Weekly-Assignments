02969b9118673111a33904d0651e40664f444f55
Nandinis1024 <114241516+Nandinis1024@users.noreply.github.com>
2022-12-30 09:02:03 +0000

    Fri, Dec 30, 2022, 9:02 AM +00:00

--- sentimental-credit/credit.py
+++ sentimental-credit/credit.py
@@ -5,18 +5,15 @@ from cs50 import get_int
 
 def main():
     number = int(input("Number: "))
-
-
-    sum1 = 0
-    while(number > 0):
-        digit = number % 10
-        sum1 = sum1 + digit
-        number = int(number / 100)
-    print(sum1)
-
-
-
     
+
+
+
+
+
+
+
+
 
 
 
