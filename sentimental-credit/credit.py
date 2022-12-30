60110058385911d72b742c6664cd8b6b5af898a1
Nandinis1024 <114241516+Nandinis1024@users.noreply.github.com>
2022-12-30 09:06:56 +0000

    Fri, Dec 30, 2022, 9:06 AM +00:00

--- sentimental-credit/credit.py
+++ sentimental-credit/credit.py
@@ -5,10 +5,13 @@ from cs50 import get_int
 
 def main():
     i = 1
+    sum1 = 0
+    sum2 = 0
     number = int(input("Number: "))
     while(number > 0):
         if (i % 2 != 0):
             digit1 = number % pow(10,i)
+            sum1 = sum1 + digit1
 
 
 
