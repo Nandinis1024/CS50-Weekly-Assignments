6432c00d148ba0369e5143bceb9da13620d99b2d
Nandinis1024 <114241516+Nandinis1024@users.noreply.github.com>
2022-12-30 15:14:49 +0000

    Fri, Dec 30, 2022, 3:14 PM +00:00

--- sentimental-readability/readability.py
+++ sentimental-readability/readability.py
@@ -3,6 +3,7 @@ from cs50 import get_string
 def main():
     text = get_string("text: ")
     print(count_letters(text))
+    print(count_words(text))
 
 def count_letters(text):
     count = 0
@@ -13,7 +14,17 @@ def count_letters(text):
             continue
     return count
 
-#def count_words(text):
+def count_words(text):
+
+    count = 0
+    for c in text:
+        if c == ' ':
+            count = count + 1
+
+        else:
+            continue
+    return count + 1
+
 #def count_sentences(text):
 
 
