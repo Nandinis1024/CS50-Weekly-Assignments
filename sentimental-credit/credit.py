6a862ffd6c7ed895f260f883bde7811ed4ce758c
Nandinis1024 <114241516+Nandinis1024@users.noreply.github.com>
2022-12-30 09:04:25 +0000

    Fri, Dec 30, 2022, 9:04 AM +00:00

--- sentimental-credit/credit.py
+++ sentimental-credit/credit.py
@@ -4,8 +4,13 @@ from cs50 import get_int
 
 
 def main():
+    i = 1
     number = int(input("Number: "))
-    
+    while(number > 0):
+        if (i % 2 != 0):
+            digit1 = number % 10
+
+
 
 
 
