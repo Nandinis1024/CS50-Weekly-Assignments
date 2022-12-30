670066756cd12c40d2aa3a222e617b5ebf665936
Nandinis1024 <114241516+Nandinis1024@users.noreply.github.com>
2022-12-30 15:23:27 +0000

    Fri, Dec 30, 2022, 3:23 PM +00:00

--- sentimental-readability/readability.py
+++ sentimental-readability/readability.py
@@ -4,6 +4,7 @@ def main():
     text = get_string("text: ")
     print(count_letters(text))
     print(count_words(text))
+    print(count_sentences(text))
 
 def count_letters(text):
     count = 0
@@ -25,7 +26,16 @@ def count_words(text):
             continue
     return count + 1
 
-#def count_sentences(text):
+def count_sentences(text):
+    count  = 0
+    for c in text:
+        if c == '.' or c == '!' or c == '?':
+            count = count + 1
+        else:
+            continue
+
+    return count
+
 
 
 if __name__ == "__main__":
