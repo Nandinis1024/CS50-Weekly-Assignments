9502ba85b59513dcf60ffc593ff348fb5c2efbec
Nandinis1024 <114241516+Nandinis1024@users.noreply.github.com>
2022-12-30 12:10:35 +0000

    Fri, Dec 30, 2022, 12:10 PM +00:00

--- sentimental-credit/credit.py
+++ sentimental-credit/credit.py
@@ -6,27 +6,24 @@ from cs50 import get_int
 def main():
 
     digit1 = 0
-    digit2 = 0
-    num_digits = 0
+    i = 0
     sum_of_double_odds = 0
-    sum_of_evens = 0
-    cc_number = get_int("Number: ")
+    sum_of_evens  = 0
+    number = get_int("Number: ")
 
-    while cc_number > 0:
-        digit2 = digit1
-        digit1 = cc_number % 10
+    while number > 0:
+        digit1 = number % 10
 
-        if num_digits % 2 == 0:
+        if i % 2 == 0:
             sum_of_evens += digit1
         else:
             multiple = 2 * digit1
             sum_of_double_odds += (multiple // 10) + (multiple % 10)
 
-        cc_number //= 10
-        num_digits += 1
+        number //= 10
+        i += 1
 
-    print(sum_of_evens)
-    print(sum_of_double_odds)
+   
 
 
 
