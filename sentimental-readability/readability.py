4e414e8b43288caa0c316bf0861050ffadc787ae
Nandinis1024 <114241516+Nandinis1024@users.noreply.github.com>
2022-12-30 13:13:31 +0000

    Fri, Dec 30, 2022, 1:13 PM +00:00

--- sentimental-readability/readability.py
+++ sentimental-readability/readability.py
@@ -4,6 +4,9 @@ def main():
     text = get_string("text: ")
 
 def count_letters(text):
+    count = 0
+    for i in range(0,len(text) + 1):
+        if i
 def count_words(text):
 def count_sentences(text):
 
