90014583f3557764c26f73cdf8c95987fd05c5b5
Nandinis1024 <114241516+Nandinis1024@users.noreply.github.com>
2022-12-31 12:15:03 +0000

    Sat, Dec 31, 2022, 12:15 PM +00:00

--- dna/practice.py
+++ dna/practice.py
@@ -1,8 +1,12 @@
 import csv
 import sys
 
-subsequence =['AGATC','TTTTTTCT','AATG','TCTAG','GATA','TATC','GAAA','TCTG']
-for i in subsequence:
-    print(i)
+with open(argv[1], mode="r") as database:
+        reader = csv.DictReader(database)
+        # Populate list of Short Tandem Repeats (STRs)
+        STRs = reader.fieldnames[1:]
+        for row in reader:
+            # Add person to profiles
+            profiles.append(row)
 
 
