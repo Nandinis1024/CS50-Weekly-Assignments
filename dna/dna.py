a6373dcfb954fc5b9ca82189e5080a3e4deecd0d
Nandinis1024 <114241516+Nandinis1024@users.noreply.github.com>
2022-12-31 11:07:03 +0000

    Sat, Dec 31, 2022, 11:07 AM +00:00

--- dna/dna.py
+++ dna/dna.py
@@ -19,11 +19,11 @@ def main():
 
 
     # TODO: Read DNA sequence file into a variable
-    sequence = []
+    seq = []
     filename2 = sys.argv[2]
     with open(filename2) as file2:
         for row in file2:
-            sequence.append(row)
+            seq.append(row)
 
 
     # TODO: Find longest match of each STR in DNA sequence
@@ -71,4 +71,4 @@ def longest_match(sequence, subsequence):
     return longest_run
 
 
-main()
+ma
