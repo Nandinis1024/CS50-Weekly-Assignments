084029e273f17bc482d699607c14cb971486228a
Nandinis1024 <114241516+Nandinis1024@users.noreply.github.com>
2022-12-31 10:34:03 +0000

    Sat, Dec 31, 2022, 10:34 AM +00:00

--- dna/practice.py
+++ dna/practice.py
@@ -2,8 +2,9 @@ import csv
 import sys
 
 database = []
-filename = /workspaces/114241516/dna/databases/small.csv  bnk sddd'df
+filename = "/workspaces/114241516/dna/databases/small.csv"
 with open(filename) as file:
     reader = csv.DictReader(file)
     for row in reader:
         database.append(row)
+print (database)
