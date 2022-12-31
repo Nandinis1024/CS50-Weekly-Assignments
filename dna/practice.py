d8f387f3ea12c1315ab378714de9de13e5a67b27
Nandinis1024 <114241516+Nandinis1024@users.noreply.github.com>
2022-12-31 09:55:19 +0000

    Sat, Dec 31, 2022, 9:55 AM +00:00

---
+++ dna/practice.py
@@ -0,0 +1,9 @@
+import csv
+import sys
+
+database = []
+filename = /workspaces/114241516/dna/databases/small.csv
+with open(filename) as file:
+    reader = csv.DictReader(file)
+    for row in reader:
+        database.append(row)
