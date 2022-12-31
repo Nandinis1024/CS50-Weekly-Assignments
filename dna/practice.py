65abe7b804433f256505c0f5cbba711ded987057
Nandinis1024 <114241516+Nandinis1024@users.noreply.github.com>
2022-12-31 20:15:31 +0000

    Sat, Dec 31, 2022, 8:15 PM +00:00

--- dna/practice.py
+++ dna/practice.py
@@ -3,8 +3,9 @@ import sys
 
 
 database = []
-    filename1 = sys.argv[1]
-    with open(filename1) as file:
-        reader = csv.DictReader(file)
-        for row in reader:
-            database.append(row) n
+filename1 = 
+with open(filename1) as file:
+    reader = csv.DictReader(file)
+    for row in reader:
+        database.append(row)
+print(database)
