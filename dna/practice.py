81b0220f99b72c69fdf0cfd5aa2fc07dc9d698c5
Nandinis1024 <114241516+Nandinis1024@users.noreply.github.com>
2022-12-31 12:41:36 +0000

    Sat, Dec 31, 2022, 12:41 PM +00:00

--- dna/practice.py
+++ dna/practice.py
@@ -8,7 +8,7 @@ with open(file) as database:
         reader = csv.DictReader(database)
 
 
-        for row in reader[1:]:
+        for row in reader_obj[1:]:
             # Add person to profiles
             profiles.append(row)
 
