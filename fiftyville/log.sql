8e0156e411505f907673681e8dd9b876d04c1c72
Nandinis1024 <114241516+Nandinis1024@users.noreply.github.com>
2023-01-03 12:57:57 +0000

    Tue, Jan 3, 2023, 12:57 PM +00:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -6,4 +6,6 @@ SELECT * FROM crime_scene_reports WHERE day = 28 AND month = 7 AND year = 2021 A
 SELECT * FROM interviews  WHERE day = 28 AND month = 7 AND year = 2021;
 --QUERY 3: To check the bakery_security_logs within 10:15-10:25 time frame
 SELECT * FROM bakery_security_logs WHERE year = 2021 AND month = 7 AND day= 28 AND hour = 10
+--QUERY 4: To check the atm_transaction
+SELECT * FROM atm_transactions WHERE year = 2021 AND month = 7 AND day = 28 AND atm_location = 'Leggett Street';
 
