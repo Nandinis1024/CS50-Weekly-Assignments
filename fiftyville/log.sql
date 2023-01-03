ea8310d23cef0bc2220ed59647ec1b58671e11f5
Nandinis1024 <114241516+Nandinis1024@users.noreply.github.com>
2023-01-03 18:55:22 +0000

    Tue, Jan 3, 2023, 6:55 PM +00:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -32,8 +32,12 @@ SELECT * FROM phone_calls WHERE caller IN ('(829) 555-5269', '(389) 555-5198', '
 --two suspects remain Diana and Bruce
 --QUERY 9: Look for the details of the eariest flight on 29
 SELECT * FROM flights WHERE year = 2021 AND month = 7 AND day = 29;
---QUERY 10: Check for the airport_id of the earliest flight
-SELECT * FROM airports WHERE id = 
+--QUERY 10: Check for the destination airport_id of the earliest flight
+SELECT * FROM airports WHERE id = 6;
+--thief is escaping to Boston
+--QUERY 10: Check for the original airport_id of the earliest flight
+SELECT * FROM airports WHERE id = 8;
+
 
 
 
