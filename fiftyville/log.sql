6b1ddd9be91a5809ebcc4b6d566fce4bd2b3cf0e
Nandinis1024 <114241516+Nandinis1024@users.noreply.github.com>
2023-01-03 18:13:02 +0000

    Tue, Jan 3, 2023, 6:13 PM +00:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -18,6 +18,12 @@ SELECT * FROM people WHERE id IN
     SELECT account_number FROM atm_transactions WHERE year = 2021 AND month = 7 AND day = 28 AND atm_location = 'Leggett Street' AND transaction_type = 'withdraw'
 
 ));
---QUERY 7: Tally the license plate from the bakery's record
 
+--QUERY 7: Tally the license plate from the bakery's record
+SELECT license_plate FROM people WHERE id IN
+(SELECT person_id FROM bank_accounts WHERE account_number IN
+(
+    SELECT account_number FROM atm_transactions WHERE year = 2021 AND month = 7 AND day = 28 AND atm_location = 'Leggett Street' AND transaction_type = 'withdraw'
+
+));
 
