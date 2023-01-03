23d5921b7d0e7db63816bec957a5b0b036cc4a17
Nandinis1024 <114241516+Nandinis1024@users.noreply.github.com>
2023-01-03 13:07:55 +0000

    Tue, Jan 3, 2023, 1:07 PM +00:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -11,4 +11,5 @@ SELECT * FROM atm_transactions WHERE year = 2021 AND month = 7 AND day = 28 AND 
 --QUERY 5:To check the bank accounts
 SELECT * FROM bank_accounts WHERE account_number IN
 (SELECT account_number FROM atm_transactions WHERE year = 2021 AND month = 7 AND day = 28 AND atm_location = 'Leggett Street' AND transaction_type = 'withdraw');
-
+--QUERY 6:TALLY the license plate number
+SELECT 
