c879f20f9d52d8eb1366be89fcb9f826bb866579
Nandinis1024 <114241516+Nandinis1024@users.noreply.github.com>
2023-01-03 12:35:46 +0000

    Tue, Jan 3, 2023, 12:35 PM +00:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -1,5 +1,7 @@
 -- Keep a log of any SQL queries you execute as you solve the mystery.
 --QUERY 1: To read the description of the crime
-SELECT * FROM crime_scene_reports WHERE day = 28 AND month = 7 AND street = 'Humphrey Street';
+SELECT * FROM crime_scene_reports WHERE day = 28 AND month = 7 AND year = 2021 AND street = 'Humphrey Street';
 --time of the crime: 10:15 am
+--QUERY 2: To read the interview transcripts for the crime
+SELECT * FROM interviews  WHERE day = 28 AND month = 7 AND year = 2021;
 
