6be74875bdf029b139b5203cb754cbc8e995b192
Nandinis1024 <114241516+Nandinis1024@users.noreply.github.com>
2023-02-07 16:53:20 +0000

    Tue, Feb 7, 2023, 10:23 PM +05:30

--- birthdays/app.py
+++ birthdays/app.py
@@ -26,6 +26,11 @@ def after_request(response):
 def index():
     if request.method == "POST":
 
+        id = request.form.get("id")
+        if id:
+            db.execute("DELETE FROM registrants WHERE id = ?", id)
+        return redirect("/")
+
         # TODO: Add the user's entry into the database
         name = request.form.get("name")
         month = request.form.get("month")
