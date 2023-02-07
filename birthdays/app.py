6c26729f8fca35652d997c314bb0ced9d93ab55b
Nandinis1024 <114241516+Nandinis1024@users.noreply.github.com>
2023-02-07 15:34:54 +0000

    Tue, Feb 7, 2023, 9:04 PM +05:30

--- birthdays/app.py
+++ birthdays/app.py
@@ -37,8 +37,6 @@ def index():
     else:
 
         # TODO: Display the entries in the database on index.html
-        people = db.execute("SELECT * FROM birthdays")
-
-        return render_template("index.html", people = people)
+        
 
 
