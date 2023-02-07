c80cd3ab21b14dbe85c04217e8d00492aa80cba4
Nandinis1024 <114241516+Nandinis1024@users.noreply.github.com>
2023-02-07 17:03:43 +0000

    Tue, Feb 7, 2023, 10:33 PM +05:30

--- birthdays/app.py
+++ birthdays/app.py
@@ -22,20 +22,25 @@ def after_request(response):
     return response
 
 
-    
+
 
 
 @app.route("/", methods=["GET", "POST"])
 def index():
     if request.method == "POST":
+        id = request.form.get("id")
+        if id:
+            db.execute("DELETE FROM birthdays WHERE id = ?", id)
+        return redirect("/")
 
 
 
         # TODO: Add the user's entry into the database
-        name = request.form.get("name")
-        month = request.form.get("month")
-        day = request.form.get("day")
-        db.execute("INSERT INTO birthdays (name, month, day) VALUES(?, ?, ?)", name, month, day)
+        else:
+            name = request.form.get("name")
+            month = request.form.get("month")
+            day = request.form.get("day")
+            db.execute("INSERT INTO birthdays (name, month, day) VALUES(?, ?, ?)", name, month, day)
 
         return redirect("/")
 
