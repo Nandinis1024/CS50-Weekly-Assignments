7b694daf0c5777b6b3227c4852588f070713db23
Nandinis1024 <114241516+Nandinis1024@users.noreply.github.com>
2023-02-07 17:55:18 +0000

    Tue, Feb 7, 2023, 11:25 PM +05:30

--- birthdays/app.py
+++ birthdays/app.py
@@ -46,12 +46,5 @@ def index():
         people = db.execute("SELECT * FROM birthdays")
         return render_template("index.html", people=people)
 
-@app.route("/", methods=["POST"])
-def deregister():
 
-    # Forget registrant
-    id = request.form.get("id")
-    if id:
-        db.execute("DELETE FROM birthdays WHERE id = ?", id)
-    return redirect("/")
 
