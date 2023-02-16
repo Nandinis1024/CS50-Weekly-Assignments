a4463c38b11a25e81bb16b64495a55e4c5417a61
Nandinis1024 <114241516+Nandinis1024@users.noreply.github.com>
2023-02-16 08:10:43 +0000

    Thu, Feb 16, 2023, 1:40 PM +05:30

--- finance/app.py
+++ finance/app.py
@@ -66,10 +66,6 @@ def register():
         if not request.form.get("username"):
             return apology("must provide username", 400)
 
-        # Check is username already taken
-        rows = db.execute("SELECT * FROM users WHERE username = :username", username=username)
-        if len(rows != 0):
-            return apology("Username already taken")
 
 
         # Ensure password was submitted
