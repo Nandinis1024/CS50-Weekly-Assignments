497fa592eefecd17f3cc3d71c252864bb84e126e
Nandinis1024 <114241516+Nandinis1024@users.noreply.github.com>
2023-02-16 19:23:23 +0000

    Fri, Feb 17, 2023, 12:53 AM +05:30

--- finance/app.py
+++ finance/app.py
@@ -46,7 +46,7 @@ def index():
     profile_1 = db.execute("SELECT * FROM transactions")
 
 
-    return render_template("index.html", profile_1 = profile_1, cash = cash)
+    return render_template("index.html", profile_1 = profile_1, cash = cash, usd=usd)
 
 
 @app.route("/buy", methods=["GET", "POST"])
