3cb65bf353d408266864575411f12617ce6634cc
Nandinis1024 <114241516+Nandinis1024@users.noreply.github.com>
2023-02-17 11:30:14 +0000

    Fri, Feb 17, 2023, 5:00 PM +05:30

--- finance/app.py
+++ finance/app.py
@@ -99,6 +99,7 @@ def history():
     """Show history of transactions"""
     user_id = session["user_id"]
     stocks = db.execute("SELECT * FROM transactions WHERE user_id = ?", user_id)
+    return render_template("history.html", stocks)
 
 
 @app.route("/register", methods=["GET", "POST"])
