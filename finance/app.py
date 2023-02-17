e4d23051450fd259449b74c9a12a2c39ca803b68
Nandinis1024 <114241516+Nandinis1024@users.noreply.github.com>
2023-02-17 11:23:46 +0000

    Fri, Feb 17, 2023, 4:53 PM +05:30

--- finance/app.py
+++ finance/app.py
@@ -97,7 +97,7 @@ def buy():
 @login_required
 def history():
     """Show history of transactions"""
-    return apology("TODO")
+    
 
 @app.route("/register", methods=["GET", "POST"])
 def register():
